b9cfbb2ac24ca0bf2461ba86dcab7064b7086ca9
SRoseDev <s.rose.dev.88@gmail.com>
2019-09-29 19:48:38 -0500

    UWSGI updates to fix db not being imported when starting app

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import app
+from db import db
+
+# required because of how uwsgi starts the app
+# app.py is not directly called
+db.init(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
